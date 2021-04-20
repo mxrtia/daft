b67169028e80d9f1bcfcdb76f30fb601e28ae4c6
mxrtia <41866138+mxrtia@users.noreply.github.com>
2021-04-20 14:07:58 +0200

    exercises 1-5

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+import hashlib
+from fastapi import FastAPI, Response, status
+from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel
+from typing import Optional
+from datetime import date, datetime, timedelta
+from fastapi.encoders import jsonable_encoder
+
 
 app = FastAPI()
 app.counter = 0
-
+app.id = 1
+app.list = []
 
 class HelloResp(BaseModel):
     msg: str
@@ -11,7 +18,7 @@ class HelloResp(BaseModel):
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world!"}
 
 
 @app.get("/counter")
@@ -25,6 +32,51 @@ def hello_name_view(name: str):
     return HelloResp(msg=f"Hello {name}")
 
 
-@app.post("/method")
-def post_method():
-    return {"method": "POST"}
+@app.get("/method")
+def get_method():
+    return {"method": "GET"}
+    
+
+@app.get("/auth")
+def auth_method(password: str, password_hash: str, response: Response):
+    hash = hashlib.sha512(password.encode())
+    if hash.hexdigest() == password_hash:
+        response.status_code = status.HTTP_204_NO_CONTENT
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class Item(BaseModel):
+    id: Optional[int]
+    name: str
+    surname: str
+    register_date: Optional[date]
+    vaccination_date: Optional[date]
+    
+    
+@app.post("/register")
+async def register(item: Item, response: Response):
+    item.id=app.id
+    item.register_date=date.today()
+    vaccination=len(item.name)+len(item.surname)
+    item.vaccination_date=date.today()+timedelta(vaccination)
+    
+    app.id +=1
+    app.list.append(item)
+    response.status_code = status.HTTP_201_CREATED
+    return jsonable_encoder(item)
+
+
+@app.get("/patient/{id}")
+async def getinfo(id: int, response: Response):
+    if not app.list:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    elif id>=1:
+        for i in app.list:
+            if i.id == id:
+                response.status_code = status.HTTP_200_OK
+                return jsonable_encoder(i)
+            else:
+                response.status_code = status.HTTP_404_NOT_FOUND
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 from fastapi.testclient import TestClient
 
 import pytest
-
+import json
+from datetime import date, datetime, timedelta
 from main import app
 
 client = TestClient(app)
@@ -10,7 +11,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello world!"}
 
 
 @pytest.mark.parametrize("name", ["Zenek", "Marek", "Alojzy Niezdąży"])
@@ -32,5 +33,49 @@ def test_counter():
     response = client.get(f"/counter")
     assert response.status_code == 200
     assert response.text == "3"
+    
+ 
+def test_method_get():
+    response = client.get("/method")
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
 
+
+def test_auth():
+    response = client.get(f"/auth?password=haslo&password_hash=013c6889f799cd986a735118e1888727d1435f7f623d05d58c61bf2cd8b49ac90105e5786ceaabd62bbc27336153d0d316b2d13b36804080c44aa6198c533215")
+    assert response.status_code == 204
+    response = client.get(f"/auth?password=haslo&password_hash=f34ad4b3ae1e2cf33092e2abb60dc0444781c15d0e2e9ecdb37e4b14176a0164027b05900e09fa0f61a1882e0b89fbfa5dcfcc9765dd2ca4377e2c794837e091")
+    assert response.status_code == 401
+    
+ 
+def test_register():
+    response = client.post("/register", json={"name": "Jan", "surname": "Nowak"})
+    assert response.status_code == 201
+    today = date.today()
+    todayplus8 = today+timedelta(len('Jan')+len('Nowak'))
+    today=today.strftime("%Y-%m-%d")
+    todayplus8=todayplus8.strftime("%Y-%m-%d")
+    assert response.json() == {'id': 1, 'name': 'Jan', 'surname': 'Nowak', 'register_date': today, 'vaccination_date': todayplus8}
+    
+    
+def test_getinfo():
+    today = date.today()
+    todayplus8 = today+timedelta(len('Jan')+len('Nowak'))
+    today=today.strftime("%Y-%m-%d")
+    todayplus8=todayplus8.strftime("%Y-%m-%d")
+    
+    response = client.get("/patient/0")
+    assert response.status_code == 400
+    
+    response = client.get("/patient/1")
+    assert response.status_code == 200
+    assert response.json() == {'id': 1, 'name': 'Jan', 'surname': 'Nowak', 'register_date': today, 'vaccination_date': todayplus8}
+    
+    response = client.get("/patient/100")
+    assert response.status_code == 404
+    
+    
+    
+
+    
     
