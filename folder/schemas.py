28d6df729e67fc1bbc35ae162ab2f940b3c90236
mxrtia <41866138+mxrtia@users.noreply.github.com>
2021-05-17 20:30:34 +0200

    Update schemas.py

--- folder/schemas.py
+++ folder/schemas.py
@@ -28,30 +28,30 @@ class Supplier2(BaseModel):
 
 
 class SupplierPost(BaseModel):
-    SupplierID: int = 0
-    CompanyName: Optional[constr(max_length=40)]
-    ContactName: Optional[constr(max_length=30)]
-    ContactTitle: Optional[constr(max_length=30)]
+    SupplierID: int
+    CompanyName: constr(max_length=60)
+    ContactName: Optional[constr(max_length=60)]
+    ContactTitle: Optional[constr(max_length=60)]
     Address: Optional[constr(max_length=60)]
-    City: Optional[constr(max_length=15)]
-    PostalCode: Optional[constr(max_length=10)]
-    Country: Optional[constr(max_length=15)]
-    Phone: Optional[constr(max_length=24)]
+    City: Optional[constr(max_length=60)]
+    PostalCode: Optional[constr(max_length=60)]
+    Country: Optional[constr(max_length=60)]
+    Phone: Optional[constr(max_length=60)]
 
     class Config:
         orm_mode = True
 
 
 class SupplierUpdate(BaseModel):
-    CompanyName: Optional[constr(max_length=40)]
-    ContactName: Optional[constr(max_length=30)]
-    ContactTitle: Optional[constr(max_length=30)]
+    CompanyName: Optional[constr(max_length=60)]
+    ContactName: Optional[constr(max_length=60)]
+    ContactTitle: Optional[constr(max_length=60)]
     Address: Optional[constr(max_length=60)]
-    City: Optional[constr(max_length=15)]
-    PostalCode: Optional[constr(max_length=10)]
-    Country: Optional[constr(max_length=15)]
-    Phone: Optional[constr(max_length=24)]
-    Fax: Optional[constr(max_length=24)] = None
+    City: Optional[constr(max_length=60)]
+    PostalCode: Optional[constr(max_length=60)]
+    Country: Optional[constr(max_length=60)]
+    Phone: Optional[constr(max_length=60)]
+    Fax: Optional[constr(max_length=60)] = None
     HomePage: Optional[str] = None
 
     class Config:
